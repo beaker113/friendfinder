3fee4c6d0999768b21929b360f5bedd932c10dbf
beaker113 <43103384+beaker113@users.noreply.github.com>
2019-02-21 18:07:07 -0800

    fixed api

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -19,9 +19,9 @@ route.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/data/friends.js'));
 });
 route.post('/api/friends', function(req, res) {
-    res.json(data.Waitlists)
+    res.json(data.waitlists)
 })
-// app.use('/api', route)
+app.use('/api', route)
 
 module.exports = route
 
